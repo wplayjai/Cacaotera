7df08ace5cc2488d739d9d3457a0d1fd696a420e
JuanYT84 <juanyt84@gmail.com>
2025-08-15 13:59:20 -0500

    jaider

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -107,7 +107,7 @@
 
             <div class="login-info">
                 <div class="info-content">
-                    <h3>Beneficios de CacaoAdmin</h3>
+                    <h3>Beneficios de AgroFinca</h3>
                     <ul class="benefits-list">
                         <li>
                             <div class="check-icon">
@@ -158,7 +158,7 @@
                                 <path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor"/>
                             </svg>
                         </div>
-                        <p>"CacaoAdmin transformó la manera en que administro mi finca. Ahora puedo ver claramente mis ganancias y planificar mejor"</p>
+                        <p>"AgroFinca transformó la manera en que administro mi finca. Ahora puedo ver claramente mis ganancias y planificar mejor"</p>
                         <div class="testimonial-author">
                             <div class="author-avatar">JM</div>
                             <span>José Martínez, Productor de Cacao</span>
